9dad7ef22df2d611ecd4becd0d723ccb5830aeae
Timothy K. Lau <timothyl@ougl-73-i.eplt.washington.edu>
2014-12-11 20:41:18 -0800

    fixed js

--- js/shapes.js
+++ js/shapes.js
@@ -1,15 +1,48 @@
-/*
-    shapes.js
-    This is where your code goes
+"use strict";
 
-    Write the code to create rectangle and circle classes that extend the
-    Shape class defined in shape.js. Then create a couple of other subclasses that
-    render different sorts of shapes using the HTML <canvas> functions.
-    http://www.w3schools.com/tags/ref_canvas.asp
+function Rectangle(left, top, width, height, stylesMap) {
+    Shape.call(this, left, top, width, height, stylesMap);
+    this.renderShape = function (canvasCtx) {
+        return canvasCtx.fillRect(this.left, this.top, this.width, this.height);
+    };
+}
 
-    You can use either the classical or the prototypal style to create your subclasses
+//code to set Rectangle.prototype
+    Rectangle.prototype = new Shape();
 
-    After you've written the code for the sublcasses, call either registerPrototypalShape()
-    or registerClassicalShape() to register your new shapes with the application. See the
-    app.js file for info on these functions.
- */
+//register new shape with the system
+    registerClassicalShape('Rectangle', Rectangle);
+
+function Circle(left, top, width, height, stylesMap) {
+    Shape.call(this, left, top, width, height, stylesMap);
+    this.renderShape = function (canvasCtx) {
+        canvasCtx.beginPath();
+        canvasCtx.arc((this.left + (this.width/2)), (this.top + (this.height/2)),(this.width/2),0,2*Math.PI);
+        return canvasCtx.fill();
+    };
+}
+
+//code to set Rectangle.prototype
+Circle.prototype = new Shape();
+
+//register new shape with the system
+registerClassicalShape('Circle', Circle);
+
+function message(left, top, width, height, stylesMap) {
+    Shape.call(this, left, top, width, height, stylesMap);
+
+    this.renderShape = function (canvasCtx) {
+        var message = "$";
+        canvasCtx.font="10px Helvetica";
+        canvasCtx.fillText(message, this.left - (5.56/2), this.top + 10);
+        console.log(canvasCtx.measureText(message));
+
+    }
+}
+
+//code to set Rectangle.prototype
+message.prototype = new Shape();
+
+//register new shape with the system
+registerClassicalShape('jQuery is so', message);
+
